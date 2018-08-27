f030ef9aa748ddecb65471d4f0b708b1987709db
mo-mo- <42301228+mo-mo-666@users.noreply.github.com>
2018-08-28 03:59:53 +0900

    Add files via upload

--- set_params.py
+++ set_params.py
@@ -3,7 +3,7 @@
 # ==============================================================================
 # set the screen size
 #
-# initial values:
+# default values:
 #     SCREEN_WIDTH = 1000
 #     SCREEN_SIZE = 600
 # Changing this params is NOT recommended if you have trouble with the screen
@@ -17,12 +17,15 @@ SCREEN_SIZE   = (SCREEN_WIDTH, SCREEN_HEIGHT)
 # ==============================================================================
 # set the block's color and the counter's color
 # BLOCK's name is not used, but do not set the same name.
+# You can use your customized color (R, G, B) .
 #
 # default values:
 #     LOWER_BLOCK = {'Name': 'Lower',
 #                    'color': BLUE}
 #     UPPER_BLOCK = {'Name': 'upper',
 #                    'color': RED}
+#     COUNT_C       = GREEN
+#     LEVEL_C       = GREEN
 # ==============================================================================
 BLACK = (  0,   0,   0)
 WHITE = (255, 255, 255)
@@ -40,15 +43,36 @@ LEVEL_C       = GREEN
 # ==============================================================================
 # set the difficulty
 #
-# NAME: the name of the level displayed (DO NOT forget '' at each side.)
-# MOVE_X: set the movement distance par frame
-# BLOCK_FREQENCY: set the block frequency [D, D+U, D+U+None] par frame, where D
+# 'NAME': the name of the level displayed (DO NOT forget '' at each side.)
+# 'MOVE_X': set the movement distance par frame
+# 'BLOCK_FREQENCY': set the block frequency [D, D+U, D+U+None] par frame, where D
 #                 is 'DOWN_BLOCK', U is 'UPPER_BLOCK, and None is 'NOT_APPEAR'
+# LEVEL: list of the LEVELs you use this game
 # CH_LEVEL: change the level when the counter of passing blocks reaches
 #           that number ((Change LEVEL(i+1) to LEVEL(i+2) when the counter
 #           reaches CH_LEVEL[i])) Be sure that CH_LEVEL[i] <= CH_LEVEL[i+1] for
 #           all i.
+# ** You can change the number of the LEVELs. **
 #
+# default values:
+#    LEVEL1 = {'NAME': 'Level 1',
+#              'MOVE_X': -3,
+#              'BLOCK_FRECENCY': [4, 5, 500]}
+#    LEVEL2 = {'NAME': 'Level 2',
+#              'MOVE_X': -5,
+#              'BLOCK_FRECENCY': [4, 5, 500]}
+#    LEVEL3 = {'NAME': 'Level 3',
+#              'MOVE_X': -5,
+#              'BLOCK_FRECENCY': [16, 20, 500]}
+#    LEVEL4 = {'NAME': 'Level 4',
+#              'MOVE_X': -8,
+#              'BLOCK_FRECENCY': [8, 10, 500]}
+#    LEVEL5 = {'NAME': 'Level 5',
+#              'MOVE_X': -8,
+#              'BLOCK_FRECENCY': [16, 20, 500]}
+#
+#    LEVEL = [LEVEL1, LEVEL2, LEVEL3, LEVEL4, LEVEL5]
+#    CH_LEVEL = [10, 30, 50, 80]
 # ==============================================================================
 
 LEVEL1 = {'NAME': 'Level 1',
